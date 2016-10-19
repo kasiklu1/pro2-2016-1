1d00d55b2e50964511a2824e289ad0ac50ed1ee0
kasiklu1 <kasiklu1@J09-07.uhk.cz>
2016-10-19 16:25:26 +0200

    prvotni model hry

---
+++ src/cz/uhk/fim/pro2/game/model/Bird.java
@@ -0,0 +1,30 @@
+package cz.uhk.fim.pro2.game.model;
+
+public class Bird {
+	
+	private String name;
+	private float positionX, positionY;
+	private float speed;
+	private int lives;
+
+	public void goUP(){
+		
+	}
+	
+	public void catchHeart(){
+		
+	}
+	
+	public void die() {
+		
+	}
+	
+	public void addLive(){
+		
+	}
+	
+	public void removeLive(){
+		
+	}
+}
+
